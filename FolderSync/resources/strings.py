d687174067624e2dcc6bcd8b23f94a6c6ebe14eb
Selmen Achour <55102728+SelmenAch@users.noreply.github.com>
2022-09-09 20:21:42 +0200

    Add strings.py

---
+++ FolderSync/resources/strings.py
@@ -0,0 +1,14 @@
+# SETTINGS.INI
+MAIN_SETTINGS = 'Main_Settings'
+SOURCE_DIR = 'SourceDirectory'
+REPLICA_DIR = 'ReplicaDirectory'
+LOG_FILE = 'LogFile'
+# SUPPORTED HASHES
+H_MD5 = 'md5'
+H_SHA_1 = 'sha1'
+H_SHA_224 = 'sha224'
+H_SHA_256 = 'sha256'
+H_SHA_384 = 'sha384'
+H_SHA_512 = 'sha512'
+H_CRC_32 = 'crc32'
+H_ADLER_32 = 'adler32'
